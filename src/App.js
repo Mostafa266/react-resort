2ca1ce30382e9dbcbced16cdd8e955a676755b51
Mostafa Abdelazim <omarenvision@Omars-Mac-mini.local>
2020-06-09 12:19:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//Import Pages 
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+// import routing 
+import {Route, Switch, Router} from 'react-router-dom';
+// Components
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    // Documentation
+    // exact =>> mean the url is exactlly what typen mean www.react.com/home -- www.react.com/home/room if you not using Exact - router will render to component - home and room 
+    // using switch to all to us to Use route with no path to make a error page
+    <>
+    <Navbar/>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/rooms' component={Rooms}/>
+      <Route exact path='/rooms/:slug' component={SingleRoom}/>
+      <Route component={Error}/> {/*we use Switch to check if route not found navigate to ERROR page */}
+    </Switch>
+    </>
   );
 }
 
